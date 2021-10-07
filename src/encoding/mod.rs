1cc0ec385868ce5b41ad6d128d0d1782c1d44ef3
Pierre Marijon <pierre@marijon.fr>
2021-10-07 18:55:59 +0200

    Encoding trait, new decode and revcomp function

--- src/encoding/mod.rs
+++ src/encoding/mod.rs
@@ -3,6 +3,13 @@
 /* mod declaration */
 pub mod naive;
 
-pub trait Encoder<P, const B: usize> {
+/* public use */
+pub use naive::Naive;
+
+pub trait Encoding<P, const B: usize> {
     fn encode(&self, seq: &[u8]) -> [P; B];
+
+    fn decode(&self, array: [P; B]) -> Vec<u8>;
+
+    fn rev_comp(&self, array: [P; B]) -> [P; B];
 }
